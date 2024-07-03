f55b6bd0d1da3d78a6e75bc884df211a091a61a2
Parise MohanKumar <mohankumar.parise523@gmail.com>
2024-07-04 01:04:56 +0530

    15 spring web http methods GET and POST

--- src/main/java/com/telusko/springboot6tutorial/model/Product.java
+++ src/main/java/com/telusko/springboot6tutorial/model/Product.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Component;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Component
 public class Product {
 
--- src/main/java/com/telusko/springboot6tutorial/service/ProductService.java
+++ src/main/java/com/telusko/springboot6tutorial/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.telusko.springboot6tutorial.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,13 +11,23 @@ import com.telusko.springboot6tutorial.model.Product;
 @Service
 public class ProductService {
 
-	List<Product> products = Arrays.asList(
+	List<Product> products = new ArrayList<>(Arrays.asList(
 		new Product(1, "Mouse", 350),
 		new Product(2, "Keyboard", 500),
 		new Product(3, "Laptop", 90000)
-	);
+	));
 
 	public List<Product> getProducts() {
 		return products;
+	}
+
+	public Product getProductByID(int prodId) {
+		return products.stream()
+		.filter(p -> p.getProdId() == prodId)
+		.findFirst().orElse(new Product(0, "No product", 0));
+	}
+
+	public void addProduct(Product product) {
+		products.add(product);
 	}
 }
