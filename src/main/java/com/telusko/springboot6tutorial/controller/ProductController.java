cb30483a79d6ad7cf3fd745b03e0fd235e386df5
Parise MohanKumar <mohankumar.parise523@gmail.com>
2024-07-04 01:42:39 +0530

    16 spring web http methods PUT and DELETE

--- src/main/java/com/telusko/springboot6tutorial/controller/ProductController.java
+++ src/main/java/com/telusko/springboot6tutorial/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.telusko.springboot6tutorial.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,18 +20,33 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 	
+	// get all products
 	@GetMapping("/products")
 	public List<Product> getProducts() {
 		return service.getProducts();
 	}
 
+	// get specific product
 	@GetMapping("/product/{prodId}")
 	public Product getProduct(@PathVariable int prodId) {
 		return service.getProductByID(prodId);
 	}
 
+	// post/add product
 	@PostMapping("/product")
 	public void addProduct(@RequestBody Product product) {
 		service.addProduct(product);
+	}
+
+	// Put / Update product
+	@PutMapping("/product")
+	public void updateProduct(@RequestBody Product product) {
+		service.updateProduct(product);
+	}
+
+	// Delete Product
+	@DeleteMapping("/product/{prodId}")
+	public void deleteProduct(@PathVariable int prodId) {
+		service.deleteProduct(prodId);
 	}
 }
