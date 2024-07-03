c6bf2b3dfe83f47d3491d5850336a3eb5683eab8
Parise MohanKumar <mohankumar.parise523@gmail.com>
2024-07-03 23:10:52 +0530

    initial commit

---
+++ src/main/java/com/telusko/springboot6tutorial/Springboot6tutorialApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.springboot6tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot6tutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot6tutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telusko/springboot6tutorial/Springboot6tutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.springboot6tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot6tutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
